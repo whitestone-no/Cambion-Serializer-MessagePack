88e59467b74b97a1574b2034742770e25e2c8a52
Gina Therese Hvidsten <8248605+gthvidsten@users.noreply.github.com>
2025-03-18 17:23:19 +0100

    Update expected values

--- test/IntegrationTests/MessagePackSerializerTests.cs
+++ test/IntegrationTests/MessagePackSerializerTests.cs
@@ -19,7 +19,7 @@ namespace Whitestone.Cambion.Serializer.MessagePack.IntegrationTests
         [Fact]
         public async Task SerializeAsync_DefaultObject_Success()
         {
-            const string expectedBase64 = "yAFAZNlyV2hpdGVzdG9uZS5DYW1iaW9uLlR5cGVzLk1lc3NhZ2VXcmFwcGVyLCBXaGl0ZXN0b25lLkNhbWJpb24sIFZlcnNpb249Ni4wLjAuMCwgQ3VsdHVyZT1uZXV0cmFsLCBQdWJsaWNLZXlUb2tlbj1udWxshaREYXRh0gAAAAGoRGF0YVR5cGXZZ1N5c3RlbS5JbnQzMiwgU3lzdGVtLlByaXZhdGUuQ29yZUxpYiwgVmVyc2lvbj04LjAuMC4wLCBDdWx0dXJlPW5ldXRyYWwsIFB1YmxpY0tleVRva2VuPTdjZWM4NWQ3YmVhNzc5OGWsUmVzcG9uc2VUeXBlwKtNZXNzYWdlVHlwZQCtQ29ycmVsYXRpb25JZNkkMDAwMDAwMDAtMDAwMC0wMDAwLTAwMDAtMDAwMDAwMDAwMDAw";
+            const string expectedBase64 = "yAFAZNlyV2hpdGVzdG9uZS5DYW1iaW9uLlR5cGVzLk1lc3NhZ2VXcmFwcGVyLCBXaGl0ZXN0b25lLkNhbWJpb24sIFZlcnNpb249NS4wLjAuMCwgQ3VsdHVyZT1uZXV0cmFsLCBQdWJsaWNLZXlUb2tlbj1udWxshaREYXRh0gAAAAGoRGF0YVR5cGXZZ1N5c3RlbS5JbnQzMiwgU3lzdGVtLlByaXZhdGUuQ29yZUxpYiwgVmVyc2lvbj04LjAuMC4wLCBDdWx0dXJlPW5ldXRyYWwsIFB1YmxpY0tleVRva2VuPTdjZWM4NWQ3YmVhNzc5OGWsUmVzcG9uc2VUeXBlwKtNZXNzYWdlVHlwZQCtQ29ycmVsYXRpb25JZNkkMDAwMDAwMDAtMDAwMC0wMDAwLTAwMDAtMDAwMDAwMDAwMDAw";
 
             MessageWrapper messageWrapper = new()
             {
@@ -49,7 +49,7 @@ namespace Whitestone.Cambion.Serializer.MessagePack.IntegrationTests
                 ResponseType = null
             };
 
-            byte[] data = Convert.FromBase64String("yAFAZNlyV2hpdGVzdG9uZS5DYW1iaW9uLlR5cGVzLk1lc3NhZ2VXcmFwcGVyLCBXaGl0ZXN0b25lLkNhbWJpb24sIFZlcnNpb249Ni4wLjAuMCwgQ3VsdHVyZT1uZXV0cmFsLCBQdWJsaWNLZXlUb2tlbj1udWxshaREYXRh0gAAAAGoRGF0YVR5cGXZZ1N5c3RlbS5JbnQzMiwgU3lzdGVtLlByaXZhdGUuQ29yZUxpYiwgVmVyc2lvbj04LjAuMC4wLCBDdWx0dXJlPW5ldXRyYWwsIFB1YmxpY0tleVRva2VuPTdjZWM4NWQ3YmVhNzc5OGWsUmVzcG9uc2VUeXBlwKtNZXNzYWdlVHlwZQCtQ29ycmVsYXRpb25JZNkkMDAwMDAwMDAtMDAwMC0wMDAwLTAwMDAtMDAwMDAwMDAwMDAw");
+            byte[] data = Convert.FromBase64String("yAFAZNlyV2hpdGVzdG9uZS5DYW1iaW9uLlR5cGVzLk1lc3NhZ2VXcmFwcGVyLCBXaGl0ZXN0b25lLkNhbWJpb24sIFZlcnNpb249NS4wLjAuMCwgQ3VsdHVyZT1uZXV0cmFsLCBQdWJsaWNLZXlUb2tlbj1udWxshaREYXRh0gAAAAGoRGF0YVR5cGXZZ1N5c3RlbS5JbnQzMiwgU3lzdGVtLlByaXZhdGUuQ29yZUxpYiwgVmVyc2lvbj04LjAuMC4wLCBDdWx0dXJlPW5ldXRyYWwsIFB1YmxpY0tleVRva2VuPTdjZWM4NWQ3YmVhNzc5OGWsUmVzcG9uc2VUeXBlwKtNZXNzYWdlVHlwZQCtQ29ycmVsYXRpb25JZNkkMDAwMDAwMDAtMDAwMC0wMDAwLTAwMDAtMDAwMDAwMDAwMDAw");
 
             MessageWrapper actual = await _serializer.DeserializeAsync(data);
 
